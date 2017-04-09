7f704deca6da9f4cca83268e9e176b2066e1a2e5
Aashay Singhal <aashay225@gmail.com>
2017-04-10 00:02:19 +0530

    corrected 404 part too

--- server.py
+++ server.py
@@ -7,15 +7,17 @@ HOST, PORT = '', 8888
 class server(Thread):
     def __init__(self, client_connection, client_address): 
         Thread.__init__(self) 
-        self.run(client_connection, client_address)
+        self.client_connection = client_connection
+        self.client_address = client_address
 
-    def run(self, client_connection, client_address):
+    def run(self):
         while True:
-            request = client_connection.recv(1024)
+            request = self.client_connection.recv(1024)
             print "requested"
 			# Checking the condition for empty requests (which we were getting while testing)
             if len(request) == 0:
-                print "empty"
+                # print "epmty request", " = = ",request
+                # print "empty"
                 continue
 
             request_head, request_body = request.split('\r\n', 1)
@@ -45,18 +47,18 @@ class server(Thread):
 
                 response_headers_raw = ''.join('%s: %s\n' % (k, v) for k, v in response_headers.iteritems())
 
-                client_connection.sendall(http_response1)
-                client_connection.send(response_headers_raw)
-                client_connection.send('\n')
-                client_connection.sendall(http_responseFile)
-                break
+                self.client_connection.sendall(http_response1)
+                self.client_connection.send(response_headers_raw)
+                self.client_connection.send('\n')
+                self.client_connection.sendall(http_responseFile)
+                # break
             else:
                 http_response1 = """HTTP/1.1 404 Not Found\n"""
 
-                request_headers = dict(x.split(': ', 1) for x in request_head[1:])
+                # request_headers = dict(x.split(': ', 1) for x in request_head[1:])
 
                 http_response2 = '\n'
-                request_method, request_uri, request_proto = request_headline.split(' ', 3)
+                # request_method, request_uri, request_proto = request_headline.split(' ', 3)
                 response_headers = {
                         'Content-Type': 'text/html; encoding=utf8',
                         'Content-Length': len(http_response2),
@@ -65,10 +67,12 @@ class server(Thread):
 
                 response_headers_raw = ''.join('%s: %s\n' % (k, v) for k, v in response_headers.iteritems())
 
-                client_connection.sendall(http_response1)
-                #client_connection.send(response_headers_raw)
-                client_connection.sendall(http_response2)
-            client_connection.close()
+                self.client_connection.sendall(http_response1)
+                self.client_connection.send(response_headers_raw)
+                self.client_connection.send('\n')
+                self.client_connection.sendall(http_response2)
+                # break
+        self.client_connection.close()
 
 listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -81,6 +85,7 @@ while True:
     print "Multithreaded Python server : Waiting for connections from TCP clients..."  
     client_connection, client_address = listen_socket.accept()
     newthread = server(client_connection, client_address) 
+    newthread.start()
     # print "port = ",port
     # newthread.start(client_connection, client_address) 
     threads.append(newthread) 
