638ff2446702018bcbd682ba6d517a273bd040f8
Aashay Singhal <aashay225@gmail.com>
2017-04-11 03:44:51 +0530

    DoS attack handling, POST/PUT requests and basic authentication

---
+++ client.py
@@ -0,0 +1,33 @@
+import socket
+import sys
+ 
+args = sys.argv
+print args
+PORT = int(args[2])
+HOST = args[1]
+GET = '/'+str(args[3])
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ 
+sock.connect((HOST, PORT))
+username = raw_input('Username : ')
+password = raw_input('Password : ')
+params = "/username="+username+"&"+"password="+password
+# print ("POST %s HTTP/1.1\r\nHost: %s\r\n" % (params, HOST))
+# sock.send("GET %s HTTP/1.0\r\nHost: %s\r\n" % (GET, HOST))
+sock.send("POST %s HTTP/1.1\r\nHost: %s\r\n" % (params, HOST))
+# print "Qwe"
+# sock.send("GET %s HTTP/1.0\r\nHost: %s\r\n" % (GET, HOST))
+ 
+data = sock.recv(1024*1024)
+# print data
+# string = ""
+# while len(data):
+#     string = string + data
+#     print "babu"
+#     data = sock.recv(1024)
+#     print "babu2"
+print data
+sock.close()
+ 
+ 
+sys.exit(0)
--- server.py
+++ server.py
@@ -4,74 +4,140 @@ from threading import Thread
 
 HOST, PORT = '', 8888
 
-class server(Thread):
-    def __init__(self, client_connection, client_address): 
+class server(Thread) :
+    def __init__(self, client_connection, client_address) : 
         Thread.__init__(self) 
         self.client_connection = client_connection
         self.client_address = client_address
 
+    def putResponse(self, request) :
+        request_head, request_body = request.split('\r\n', 1)
+        request_head = request_head.splitlines()
+        request_headline = request_head[0]
+        fileName = '.' + request_headline.split(' ')[1]
+        request_message = request.split('\r\n\r\n')[1]
+
+        f = open(fileName, 'w')
+        f.write(request_message)
+        http_response1 = """HTTP/1.1 200 OK\n"""
+        request_method, request_uri, request_proto = request_headline.split(' ', 3)
+        response_headers = {
+                'Content-Type': 'text/html; encoding=utf8',
+                'Content-Length': 0,
+                'Connection': 'close',
+            }
+
+
+        response_headers_raw = ''.join('%s: %s\n' % (k, v) for k, v in response_headers.iteritems())
+        sendData = http_response1+response_headers_raw+'\n'
+        self.client_connection.sendall(sendData)
+
+    def MYauth(self, username, password) :
+        if username == "asd" and password == "123":
+            return True
+        return False
+    def postResponse(self, request) :
+        request_head, request_body = request.split('\r\n', 1)
+        request_head = request_head.splitlines()
+        request_headline = request_head[0]
+        paramURL = request_headline.split(' ')[1]
+        paramURL = paramURL.split('/')
+        paramURL = paramURL[1]
+        request_message = request.split('\r\n')
+        # print "asdasd",request_message , paramURL
+
+        # params = paramURL.split('?')
+        # print "Asd",params
+        params = paramURL.split('&')
+        for p in params:
+            x = p.split('=')
+            exec(x[0] + '=' + 'x[1]')
+        # print username , password
+        # if username == "asd" and password == "123":
+        if self.MYauth(username,password) :
+            http_response1 = """HTTP/1.1 200 OK\n"""
+            request_method, request_uri, request_proto = request_headline.split(' ', 3)
+            response_headers = {
+                    'Content-Type': 'text/html; encoding=utf8',
+                    'Content-Length': 0,
+                    'Connection': 'close',
+                }
+
+
+            response_headers_raw = ''.join('%s: %s\n' % (k, v) for k, v in response_headers.iteritems())
+            sendData = http_response1+'\n'
+            self.client_connection.sendall(sendData)
+            # print "Asdasdasdasd"
+
+
+
+
+
+    def getResponse(self, request) :
+        request_head, request_body = request.split('\r\n', 1)
+        request_head = request_head.splitlines()
+        request_headline = request_head[0]
+        fileName = '.' + request_headline.split(' ')[1]
+
+        # Checking if file is in the current directory
+        if os.path.isfile(fileName) :
+            http_response1 = """HTTP/1.1 200 OK\n"""
+
+            # Making a dict with headers and their values
+            request_headers = dict(x.split(': ', 1) for x in request_head[1:])
+
+            f = open(fileName,'rb')
+            l = f.read(1024)
+            http_responseFile = ''
+            http_responseFile = http_responseFile + l
+            f.close()
+            request_method, request_uri, request_proto = request_headline.split(' ', 3)
+            response_headers = {
+                    'Content-Type': 'text/html; encoding=utf8',
+                    'Content-Length': len(http_responseFile),
+                    'Connection': 'close',
+                }
+
+            response_headers_raw = ''.join('%s: %s\n' % (k, v) for k, v in response_headers.iteritems())
+            sendData = http_response1+response_headers_raw+'\n'+http_responseFile
+            self.client_connection.sendall(sendData)
+        else:
+            http_response1 = """HTTP/1.1 404 Not Found\n"""
+            http_response2 = '\n'
+            response_headers = {
+                    'Content-Type': 'text/html; encoding=utf8',
+                    'Content-Length': len(http_response2),
+                    'Connection': 'close',
+                }
+            response_headers_raw = ''.join('%s: %s\n' % (k, v) for k, v in response_headers.iteritems())
+
+            sendData = http_response1 + response_headers_raw + '\n' + http_response2
+            self.client_connection.sendall(sendData)
+
+
+    def respond(self, request):
+        request_head, request_body = request.split('\r\n', 1)
+        request_head = request_head.splitlines()
+
+        requestType = request_head[0].split(' ')[0]
+        if requestType == "GET" :
+            self.getResponse(request)
+        elif requestType == "POST" :
+            self.postResponse(request)
+        elif requestType == "PUT" :
+            self.putResponse(request)
+        # else :
+
+
     def run(self):
         while True:
             request = self.client_connection.recv(1024)
-            print "requested"
 			# Checking the condition for empty requests (which we were getting while testing)
             if len(request) == 0:
-                # print "epmty request", " = = ",request
-                # print "empty"
                 continue
+            # print request
+            self.respond(request)
 
-            request_head, request_body = request.split('\r\n', 1)
-            request_head = request_head.splitlines()
-            request_headline = request_head[0]
-            fileName = '.' + request_headline.split(' ')[1]
-
-            # Checking if file is in the current directory
-            if os.path.isfile(fileName) :
-                http_response1 = """HTTP/1.1 200 OK\n"""
-
-                # Making a dict with headers and their values
-                request_headers = dict(x.split(': ', 1) for x in request_head[1:])
-
-                f = open(fileName,'rb')
-                l = f.read(1024)
-                http_responseFile = ''
-                http_responseFile = http_responseFile + l
-                f.close()
-				
-                request_method, request_uri, request_proto = request_headline.split(' ', 3)
-                response_headers = {
-			            'Content-Type': 'text/html; encoding=utf8',
-			            'Content-Length': len(http_responseFile),
-			            'Connection': 'close',
-                    }
-
-                response_headers_raw = ''.join('%s: %s\n' % (k, v) for k, v in response_headers.iteritems())
-
-                self.client_connection.sendall(http_response1)
-                self.client_connection.send(response_headers_raw)
-                self.client_connection.send('\n')
-                self.client_connection.sendall(http_responseFile)
-                # break
-            else:
-                http_response1 = """HTTP/1.1 404 Not Found\n"""
-
-                # request_headers = dict(x.split(': ', 1) for x in request_head[1:])
-
-                http_response2 = '\n'
-                # request_method, request_uri, request_proto = request_headline.split(' ', 3)
-                response_headers = {
-                        'Content-Type': 'text/html; encoding=utf8',
-                        'Content-Length': len(http_response2),
-                        'Connection': 'close',
-                    }
-
-                response_headers_raw = ''.join('%s: %s\n' % (k, v) for k, v in response_headers.iteritems())
-
-                self.client_connection.sendall(http_response1)
-                self.client_connection.send(response_headers_raw)
-                self.client_connection.send('\n')
-                self.client_connection.sendall(http_response2)
-                # break
         self.client_connection.close()
 
 listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -79,15 +145,29 @@ listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 listen_socket.bind((HOST, PORT))
 print 'Serving HTTP on port %s ...' % PORT
 threads = []
-
+requesting_clients = dict()
+list_client = []
+blacklist = []
 while True: 
     listen_socket.listen(4) 
-    print "Multithreaded Python server : Waiting for connections from TCP clients..."  
+    # print "Multithreaded Python server : Waiting for connections from TCP clients..."  
     client_connection, client_address = listen_socket.accept()
+    if client_address[0] in blacklist :
+        client_connection.close()
+        continue
     newthread = server(client_connection, client_address) 
+
+    if client_address[0] in list_client : 
+        requesting_clients[client_address[0]] +=  1
+    else:
+        requesting_clients[client_address[0]] = 1
+        list_client.append(client_address[0])
+
+    print requesting_clients
+    for value in list_client : 
+        if requesting_clients[value] > 3 : 
+            blacklist.append(value)
     newthread.start()
-    # print "port = ",port
-    # newthread.start(client_connection, client_address) 
     threads.append(newthread) 
  
 for t in threads: 
